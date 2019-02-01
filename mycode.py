b0780da18766ccd7a0755037a6d13532e9479087
xudaaaaan <xudaaaaan@163.com>
2019-01-31 19:34:05 -0800

    last commit

--- mycode.py
+++ mycode.py
@@ -1,15 +1,19 @@
 import json
+import csv
+
+data_path = "./Data/"
 
 class DataObfuscator():
 	def __init__():
-
+		self.fake_file = data_path + "FakeInfo.csv"
+		self.fake_info = None
 
 	def read_file(filename):
 		"""
 		input: string (Json file name)
 		output: list of python dictionary
 		"""
-		with open(filename, "r+") as f:
+		with open(data_path + filename, "r+") as f:
 			return json.load(f)
 
 	def output_file(info, filename):
@@ -17,30 +21,68 @@ class DataObfuscator():
 		input:
 		info: list of dictionary
 		"""
-		with open(filename, "w+") as f:
+		with open(data_path + filename, "w+") as f:
 			json.dump(info, f)
-		return
+
+	def fake_info():
+		"""
+		output: List[List of each line]
+		"""
+		with open(self.fake_file) as f:
+			reader = csv.reader(f)
+			info = list(reader)[1:]
+			info = list(zip(*info))[1:] #3000 lines
+			return info
+			#sur, giv, mid, add, zipcode, email, username, phone, height, weight, dob, company, ssn
+
 
 	def name_processor(ori_name):
-		pass
+		def hash_name(name):
+			result = 0
+			for char in name:
+				result += ord(char)
+			return (result % 3000)
 
-	def ID_processor(ori_name):
-		pass
+		name = ori_name.split(" ")
+		if len(name) == 3:
+			f, m, l = hash_name(name[0]), hash_name(name[1]), hash_name(name[2])
+		fake_first = self.fake_info[0][f]
+		fake_last = self.fake_info[2][m]
+		fake_mid = self.fake_info[2][l]
+		return fake_first + fake_last + fake_mid
+		
+
+	def ID_processor(ori_id):
+		
 
 	def zip_processor(ori_zip):
-		pass
+		m = int(ori_zip) % 3000
+		return self.fake_info[4]
 
 	def SSN_processor(ori_ssn):
 		pass
 
 	def phone_processor(ori_phone):
-		pass
+		temp = str(ori_phone).split("-")
+		district, res = temp[0], temp[1:].join("")
+		fake_phone = self.fake_info[7][temp % 3000]
+		return (district + fake_phone.split("-")[1:].join(" "))
 
 	def email_processor(ori_email):
-		pass
+		def hash_email(email):
+			result = 0
+			for char in name:
+				result += ord(char)
+			return (result % 3000)
+		return self.fake_info[5][hash_email(ori_email)]
 
 	def company_processor(ori_company):
 		pass
+
+	def process_data(filename):
+		self.read_file(filename)
+		
+
 
 
 
--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import json
+import csv
 
 with open("./Data/json/data50.json") as f:
 	a = json.load(f)
 
-print(type(a)) #list
-print(len(a)) #50
-print(a[49]) #dictionary
+#print(type(a)) #list
+#print(len(a)) #50
+#print(a[49]) #dictionary
+
+with open("Data/FakeInfo.csv") as f:
+	reader = csv.reader(f)
+	b = list(reader)
+print (list(zip(*b)))
